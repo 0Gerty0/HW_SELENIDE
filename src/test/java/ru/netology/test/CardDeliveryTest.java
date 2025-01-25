ce72c577d9bfae7709a7db42bd7a579bbcd0d254
Aleks Danilushkin <thenumber@ya.ru>
2025-01-25 14:09:11 +0300

    Correction of mistakes 4

--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -13,8 +14,10 @@ public class CardDeliveryTest {
 
     @BeforeAll
     static void setUp() {
+        WebDriverManager.chromedriver().setup();
         Configuration.baseUrl = "http://localhost:9999";
         Configuration.browser = "chrome";
+        Configuration.headless = true;
         Configuration.browserSize = "1920x1080";
     }
 
@@ -35,4 +38,3 @@ public class CardDeliveryTest {
                 .shouldHave(text("Успешно! Встреча успешно забронирована на " + planningDate));
     }
 }
-
